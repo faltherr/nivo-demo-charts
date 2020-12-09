20bc4e5884fd3bb8e0d9d4d68b97d836acc64dc5
Forest Altherr <forest.altherr@biofiredx.com>
2020-12-09 11:22:11 -0700

    Custom tick labels added to pareto

--- src/BarChart.js
+++ src/BarChart.js
@@ -2,12 +2,45 @@ import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
 import data from "./paretoData";
 
-export default function BarChart() {
+// This function allows us to split italicization of pathogen labels
+const CustomTick = (tick) => {
+  const label = tick.value
+    .replace("<em>", '<tspan font-style="italic">')
+    .replace("</em>", "</tspan>");
+  const tickValue = (
+    <text
+      transform={"translate(-10,0)"}
+      dominant-baseline="central"
+      dangerouslySetInnerHTML={{ __html: label }}
+      text-anchor="end"
+      style={{
+        fontFamily: "sans-serif",
+        fontSize: "11px",
+        fill: "rgb(51, 51, 51)",
+      }}
+    />
+  );
+  return (
+    <g transform={`translate(${tick.x},${tick.y})`}>
+      <line
+        x1="0"
+        x2="-5"
+        y1="0"
+        y2="0"
+        style={{ stroke: "rgb(119, 119, 119)", strokeWidth: 1 }}
+      ></line>
+      {tickValue}
+    </g>
+  );
+};
 
+export default function BarChart() {
   return (
     <>
       <ResponsiveBar
         data={data.sort((a, b) => a.value - b.value)}
+        // Uncomment this to adjust the layers
+        // layers={["grid", "bars", "markers", "legends", "annotations", "axes"]}
         keys={["value"]}
         indexBy="id"
         // Change the margins if more space is required for labels
@@ -25,10 +58,10 @@ export default function BarChart() {
         enableLabel={false}
         // Y-axis configuration options
         axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            format: value => value
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          renderTick: CustomTick,
         }}
       />
     </>
